d61cc2305ec9ad01c79f3a88c51dab6a93579c18
Ash Wilson <smashwilson@github.com>
2017-10-06 11:24:13 -0400

    Now with real assertions

--- test/stress.test.js
+++ test/stress.test.js
@@ -41,29 +41,55 @@ function runExample (testName, callback) {
 
 describe('.stress', function () {
   it('duplicates marked specs by the specified iteration count', function (done) {
-    runExample('single', function (err, stdout, stderr) {
+    runExample('single', function (err, results, stderr) {
       assert.isNotOk(err)
+
+      assert.lengthOf(results.passes, 10)
+      for (var i = 0; i < 10; i++) {
+        assert.equal(results.passes[i].fullTitle, 'stress-testing a single spec runs multiple times #' + i)
+      }
+
       done()
     })
   })
 
   it('duplicates marked suites by the specified iteration count', function (done) {
-    runExample('suite', function (err, stdout, stderr) {
+    runExample('suite', function (err, results, stderr) {
       assert.isNotOk(err)
+
+      assert.lengthOf(results.passes, 10)
+      for (var i = 0; i < 5; i++) {
+        assert.equal(results.passes[i * 2].fullTitle, 'stress-testing a full suite #' + i + ' spec one')
+        assert.equal(results.passes[i * 2 + 1].fullTitle, 'stress-testing a full suite #' + i + ' spec two')
+      }
+
       done()
     })
   })
 
   it('implies .only', function (done) {
-    runExample('only', function (err, stdout, stderr) {
+    runExample('only', function (err, results, stderr) {
       assert.isNotOk(err)
+
+      assert.lengthOf(results.passes, 3)
+      assert.lengthOf(results.failures, 0)
+
+      for (var i = 0; i < 3; i++) {
+        assert.equal(results.passes[i].fullTitle, 'implies .only runs only this spec #' + i)
+      }
+
       done()
     })
   })
 
   it('does not interfere with unmarked specs', function (done) {
-    runExample('normal', function (err, stdout, stderr) {
+    runExample('normal', function (err, results, stderr) {
       assert.isNotOk(err)
+
+      assert.lengthOf(results.passes, 1)
+      var pass = results.passes[0]
+      assert.equal(pass.fullTitle, 'normal specs does not interfere')
+
       done()
     })
   })
